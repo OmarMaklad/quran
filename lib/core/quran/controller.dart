7013fcfc98f0bf6995324f2b60e96ad385e41f1c
AhmedAbogameel <gamilco81@gmail.com>
2021-07-03 15:20:24 +0200

    Offline arabic quran

--- lib/core/quran/controller.dart
+++ lib/core/quran/controller.dart
@@ -1,13 +1,18 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
 import 'package:quran/core/quran/surah_model.dart';
 import 'package:quran/core/storage/app_storage.dart';
 
 class QuranController {
 
   static Future<String> getSurahById(int id)async{
-    String url = 'http://api.alquran.cloud/v1/surah/$id';
-    if(!AppStorage.isAppLanguageArabic)
-      url = url + '/en.asad';
+    if(AppStorage.isAppLanguageArabic){
+      final surah = await _getSurahFromJson(id);
+      return surah;
+    }
+    String url = 'http://api.alquran.cloud/v1/surah/$id' + '/en.asad';
     final response = await Dio().get(url);
     SurahModel surahModel = SurahModel.fromJson(response.data);
     String surah = '';
@@ -17,6 +22,19 @@ class QuranController {
       if(!removed) ayah = 'بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيم' + '\n' + ayah;
       removed = true;
       surah = surah + ayah + ' (${element.numberInSurah}) ';
+    });
+    return surah;
+  }
+
+  static Future<String> _getSurahFromJson(int id)async{
+    final String response = await rootBundle.loadString('assets/ar.json');
+    final data = await json.decode(response)['AR'] as List;
+    String surah = '';
+    data.forEach((element) {
+      if(element['surah_number'] == id)
+        surah = surah + ' ${element['content']}' + ' (${element['verse_number']})';
+      else
+        return;
     });
     return surah;
   }
